592ca97919883d6984a958e4c317dcfee140f73b
Florian Dueller <florian.dueller@trinitec.at>
2022-07-27 22:59:17 +0200

    fix: Add some placeholder code to clock-2.tsx

---
+++ pages/apps/clock-2.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Clock2() {
+	return (
+			<div>
+				<Head>
+					<title>Clock 2</title>
+				</Head>
+				<div>
+					<div className={ "bg-blue-700 text-white w-100 p-4 text-center" }>
+						<h1>Clock 2</h1>
+					</div>
+					<div className={"w-full text-center text-4xl m-4"} >WIP! 🏗️</div>
+				</div>
+			</div>
+	)
+}
